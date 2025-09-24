93d73cb5e8a89e82b4f7afed3fab0c4e88b138ab
Vincenzo Matonti <vincenzomatonti94@gmail.com>
2025-09-24 14:54:59 +0200

    creazione migration per aggiungere colonna is_revisor --- creazione comando-custom app per rendere revisore un utente

---
+++ app/Console/Commands/MakeUserRevisor.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class MakeUserRevisor extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:make-user-revisor {email}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Rende un utente revisore';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //ricerca utente tramite il parametro specificato "email"
+        $user = User::where('email', $this->argument('email'))->first();
+        //se c'è mancata corrispondenza nel db messaggio di errore
+        if (!$user) {
+            $this->error('Utente non trovato');
+            return;
+        }
+        //se l'utente viene trovato il suo record viene aggiornato diventando revisore
+        $user->is_revisor = true;
+        $user->save();
+        $this->info("L'utente {$user->name} è ora revisore");
+    }
+}
