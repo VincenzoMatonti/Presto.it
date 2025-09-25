9ceef451a57b2a690a891770932dc4474edce22c
Vincenzo Matonti <vincenzomatonti94@gmail.com>
2025-09-25 16:29:19 +0200

    Continuazione traduzione sito

--- lang/it/ui.php
+++ lang/it/ui.php
@@ -1,7 +1,22 @@
 <?php
-return 
-   [
-       'subtitleHome' => '"Trova, compra, vendi: ogni giorno nuove occasioni a portata di click!"',
-       'helloUser' => 'Ciao, utente',
-   ];
- 
+return
+    [
+        'subtitleHome' => '"Trova, compra, vendi: ogni giorno nuove occasioni a portata di click!"',
+        'helloUser' => 'Ciao, utente',
+        'categories' => 'Categorie',
+        'home' => 'Home',
+        'allArticles' => 'Tutti gli articoli',
+        'login' => 'Accedi',
+        'register' => 'Registrati',
+        'logout' => 'Log out',
+        'elettronica' => 'Elettronica',
+        'abbigliamento' => 'Abbigliamento',
+        'salute e bellezza' => 'Salute e bellezza',
+        'casa e giardinaggio' => 'Casa e giardinaggio',
+        'giocattoli' => 'Giocattoli',
+        'sport' => 'Sport',
+        'animali domestici' => 'Animali domestici',
+        'libri e riviste' => 'Libri e riviste',
+        'accessori' => 'Accessori',
+        'motori' => 'Motori',
+    ];
--- resources/views/components/shared/navbar.blade.php
+++ resources/views/components/shared/navbar.blade.php
@@ -7,14 +7,14 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active mytextcolor" aria-current="page" href="{{route('homepage')}}">Home <i class="fa-solid fa-house-chimney"></i> </a>
+                    <a class="nav-link active mytextcolor" aria-current="page" href="{{route('homepage')}}">{{__('ui.home')}} <i class="fa-solid fa-house-chimney"></i> </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link mytextcolor" aria-current="page" href="{{route('index.article')}}">Tutti gli articoli <i class="fa-solid fa-newspaper"></i></a>
+                    <a class="nav-link mytextcolor" aria-current="page" href="{{route('index.article')}}">{{__('ui.allArticles')}} <i class="fa-solid fa-newspaper"></i></a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle mytextcolor" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Categorie <i class="fa-solid fa-list"></i>
+                        {{__('ui.categories')}} <i class="fa-solid fa-list"></i>
                     </a>
                     <ul class="dropdown-menu mynavbg">
                         @foreach ($categories as $category)
@@ -48,7 +48,7 @@
                         </li>
                         <hr class="dropdown-divider">
                         <li> <a href="#" class="dropdown-item mynavbg mytextcolor"
-                                onclick="event.preventDefault(); document.querySelector('#form-logout').submit();">Logout <i class="fa-solid fa-right-from-bracket mx-1"></i></a>
+                                onclick="event.preventDefault(); document.querySelector('#form-logout').submit();">{{__('ui.logout')}} <i class="fa-solid fa-right-from-bracket mx-1"></i></a>
                         </li>
                         <form action="{{route('logout')}}" method="POST" class="d-none" id="form-logout">@csrf</form>
                     </ul>
@@ -59,9 +59,9 @@
                         {{__('ui.helloUser')}} <i class="fa-solid fa-user"></i>
                     </a>
                     <ul class="dropdown-menu mynavbg">
-                        <li><a class="dropdown-item mynavbg mytextcolor" href="{{route('login')}}">Accedi<i class="fa-solid fa-door-open mx-3"></i></a></li>
+                        <li><a class="dropdown-item mynavbg mytextcolor" href="{{route('login')}}">{{__('ui.login')}}<i class="fa-solid fa-door-open mx-3"></i></a></li>
                         <hr class="dropdown-divider">
-                        <li><a class="dropdown-item mynavbg mytextcolor" href="{{route('register')}}">Registrati<i class="fa-solid fa-id-card mx-3"></i></a></li>
+                        <li><a class="dropdown-item mynavbg mytextcolor" href="{{route('register')}}">{{__('ui.register')}}<i class="fa-solid fa-id-card mx-3"></i></a></li>
                     </ul>
                 </li>
                 @endauth
